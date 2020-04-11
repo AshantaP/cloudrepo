ba1484ec4437d98f8bff49e3ff3fca07a03277a8
Ashanta Palei <ashanta.palei@gmail.com>
2020-04-11 19:05:33 +0000

    new file change

--- index.sh
+++ index.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo echo "Welcome to my first devops link" > /var/www/html/index.html
+sudo echo "Welcome to my first devops link /n Also it is my first project" > /var/www/html/index.html
